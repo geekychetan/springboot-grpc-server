f0cf63885c544b53cf3e9cbd0c1173ac31544f27
geekychetan <chetan.lohar@enterprisedb.com>
2025-01-20 13:05:09 +0530

    feat(GKC-0000): Created project structure

---
+++ src/main/java/com/geekychetan/springboot_grpc_server/SpringbootGrpcServerApplication.java
@@ -0,0 +1,13 @@
+package com.geekychetan.springboot_grpc_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGrpcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGrpcServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geekychetan/springboot_grpc_server/SpringbootGrpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekychetan.springboot_grpc_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGrpcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
